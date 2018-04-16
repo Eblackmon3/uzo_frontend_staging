e4658d45808e2c9a114203eb4f81f9c609707f78
Eric Blackmon <ericblackmon38@gmail.com>
2018-04-16 19:27:55 -0400

    chaning registration landing

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ ini_set('post_max_size', '100M');
 ini_set('upload_max_filesize', '100M'); 
 ini_set('memory_limit', '1000M'); 
 ini_set('max_execution_time', '1920');
-header( 'Location: /index.html' ); 
+header( 'Location: /index.html' )
+; 
 
 
 
