22ad7ef2c5332d48d69cbd6f147cee5f360c0862
Eric Blackmon <ericblackmon38@gmail.com>
2018-08-15 20:38:03 -0700

    company password

---
+++ src/main/java/Application/ScheduledTasks.java
@@ -0,0 +1,21 @@
+package Application;
+
+
+import Model.DataManagers.CompanyManager;
+import Model.DataManagers.StudentManager;
+import org.json.JSONArray;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTasks {
+    private CompanyManager manager;
+    private StudentManager studManager;
+
+    @Scheduled(cron = "0 33 20 1/1 * ? *")
+    public void sendTheBoystheEmail() {
+        JSONArray array= studManager.getAllStudentsEmails();
+        manager.generateAndSendEmailVariable(array);
+
+    }
+}
