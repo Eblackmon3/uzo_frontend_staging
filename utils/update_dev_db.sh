92d1dbf661fea3e7f617ae13cc36555681aeb42e
Celina Amados <celinaamados@aol.com>
2018-11-13 18:20:17 -0800

    update_dev_db.sh and enabling texting

--- utils/update_dev_db.sh
+++ utils/update_dev_db.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# add this script to a new directory /utils in uzo-web-app
-# scheduler runs ./utils/update_dev_db.sh
+# add this script to a new directory /utils
+# scheduler runs ./utils/update_dev_db.sh in uzo-web-app
 
 curl -s https://s3.amazonaws.com/assets.heroku.com/heroku-client/heroku-client.tgz | tar xz
 PATH="/app/heroku-client/bin:$PATH"
+heroku update
 heroku pg:copy MAUVE AQUA --app uzo-web-app --confirm uzo-web-app
